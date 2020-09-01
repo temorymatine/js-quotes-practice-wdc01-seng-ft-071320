28d9b60b1115f92c83d24b4de5232dbe3a95f205
chsohn15 <chsohn15@gmail.com>
2020-08-31 20:35:19 -0400

    Edit form

--- src/index.js
+++ src/index.js
@@ -5,20 +5,21 @@ const ul = document.getElementById("quote-list")
 
 const url = "http://localhost:3000/quotes?_embed=likes"
 
-fetch(url).then(resp => resp.json()).then(quotes => addAllQuotes(quotes))
-})
+fetch(url)
+.then(resp => resp.json())
+.then(quotes => addAllQuotes(quotes))
+
 
 function addAllQuotes(quotes){
-
     quotes.forEach(quote=>addQuote(quote))
-
 }
 
 function addQuote(quote){
     
-const ul = document.getElementById("quote-list")
+    const ul = document.getElementById("quote-list")
     let li = document.createElement("li")
     li.className = 'quote-card'
+    li.id = `quote-${quote.id}`
 
     let blockedQuote = document.createElement("blockquote")
     blockedQuote.className = 'blockquote'
@@ -26,10 +27,12 @@ const ul = document.getElementById("quote-list")
     let p = document.createElement('p')
     p.className = "mb-0"
     p.innerText = quote.quote
+    p.id = `p-${quote.id}`
 
     let footer = document.createElement('footer')
     footer.className =  "blockquote-footer"
     footer.innerText = quote.author
+    footer.id = `footer-${quote.id}`
 
 
     let br = document.createElement("br")
@@ -56,12 +59,13 @@ const ul = document.getElementById("quote-list")
     deleteBtn.className = "btn-danger"
     deleteBtn.innerText = "Delete"
 
+    let editBtn = document.createElement("button")
+    editBtn.className = "btn-edit"
+    editBtn.innerText = "Edit"
 
-    blockedQuote.append(p,footer,br,likeBtn,deleteBtn)
+    blockedQuote.append(p,footer,br,likeBtn,deleteBtn, editBtn)
     li.append(blockedQuote)
 
-
-    
     ul.append(li)
 
 
@@ -95,11 +99,90 @@ const ul = document.getElementById("quote-list")
             quoteSpan.innerText = num += 1
         })
 
+    })
+
+    // Create edit form
+    const editForm = document.createElement('FORM')
+    editForm.id = `edit-quote-${quote.id}`
+    editForm.style.display = "none"
+
+    let formDiv = document.createElement('div')
+    formDiv.className = "form-group"
+    formDiv.id = `form-id-${quote.id}`
+
+    let quoteLabel = document.createElement('label')
+    quoteLabel.for = "edited-quote"
+    quoteLabel.innerText = "Quote"
+
+    let quoteInput = document.createElement('input')
+    quoteInput.name = "quote"
+    quoteInput.type = "text"
+    quoteInput.class = "form-control"
+    quoteInput.id = `edited-quote-${quote.id}`
+    quoteInput.value = `${quote.quote}`
+
+    formDiv.append(quoteLabel, quoteInput)
+
     
-   
-})
+    let formDiv2 = document.createElement('div')
+    formDiv2.className = "form-group"
 
+    let authorLabel = document.createElement('label')
+    authorLabel.for = "Author"
+    authorLabel.innerText = "Author"
 
+    let authorInput = document.createElement('input')
+    authorInput.name = "author"
+    authorInput.type = "text"
+    authorInput.class = "form-control"
+    authorInput.id = `author-quote-${quote.id}`
+    authorInput.value = quote.author
+
+    let editSubmitBtn = document.createElement('button')
+    editSubmitBtn.innerText = "Submit Edits"
+
+    formDiv2.append(authorLabel, authorInput)
+    editForm.append(formDiv, formDiv2, editSubmitBtn)
+
+    li.append(editForm)
+
+    editBtn.addEventListener("click",function(){
+        let quoteEditForm = document.getElementById(`edit-quote-${quote.id}`)
+        quoteEditForm.style.display = "block"
+
+        quoteEditForm.addEventListener('submit', function(e){
+            e.preventDefault()
+            
+            let newQuoteValue = e.target[0].value
+            let newAuthorValue = e.target[1].value
+
+            let configObj = { 
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json"
+                }, 
+                body: JSON.stringify({
+                    quote: newQuoteValue,
+                    author: newAuthorValue
+                })
+            }
+
+            fetch(`http://localhost:3000/quotes/${quote.id}/?_embed=likes/`, configObj)
+            .then(res => res.json())
+            .then(updatedQuote => {
+                debugger
+                let pToUpdate = document.getElementById(`p-${updatedQuote.id}`)
+                pToUpdate.innerText = updatedQuote.quote
+                let footerToUpdate = document.getElementById(`footer-${updatedQuote.id}`)
+                footerToUpdate.innerText = updatedQuote.author
+
+                quoteEditForm.style.display = "none"
+            })
+            //debugger
+
+        })
+    })
+}
 
 
 let form = document.getElementById("new-quote-form")
@@ -123,11 +206,8 @@ form.addEventListener("submit",function(e){
     .then(resp => resp.json())
     .then(quote => addQuote(quote))
 
-    
-    // addQuote(quote)
-
+    })
 })
-}
 
 
 
